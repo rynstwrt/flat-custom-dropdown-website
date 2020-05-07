bf4ff36a4214ad332e097cf730c12e98e66446e7
Ryan Stewart <ryanstewartalex@gmail.com>
2020-05-07 13:32:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,19 @@
-function hideAll()
-{
-	document.querySelectorAll("section div").forEach(obj =>
-	{
-		obj.style.display = "none";
-	});
-}
-
 document.querySelectorAll("section p").forEach(obj =>
 {
 	obj.addEventListener("mouseover", e1 =>
 	{
-
 		const section = e1.target.parentElement;
 		const div = section.children[1];
 		div.style.display = "flex";
 
 		section.addEventListener("mouseleave", e2 =>
 		{
-			hideAll();
+			div.style.display = "none";
 		});
 
 		div.addEventListener("mouseleave", e2 =>
 		{
-			hideAll();
+			div.style.display = "none";
 		});
 	});
-
-
-
-
-
-
-
-
 });
