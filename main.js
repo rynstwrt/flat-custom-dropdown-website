6f37992e2c78105da5fd543b09b88a20178a9d3d
Ryan Stewart <ryanstewartalex@gmail.com>
2020-05-07 14:05:47 -0500

    Change ::after to span.

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
+const RESTING = " v";
+const OPEN = " >"
+
+// what's called when hover ends
+function undo(after, div)
+{
+	div.style.display = "none";
+	after.textContent = RESTING;
+}
+
 document.querySelectorAll("section p").forEach(obj =>
 {
+
+	const after = obj.children[0];
+	after.textContent = RESTING;
+
 	obj.addEventListener("mouseover", e1 =>
 	{
 		const section = e1.target.parentElement;
 		const div = section.children[1];
 		div.style.display = "flex";
+		after.textContent = OPEN;
 
 		section.addEventListener("mouseleave", e2 =>
 		{
-			div.style.display = "none";
+			undo(after, div);
 		});
 
 		div.addEventListener("mouseleave", e2 =>
 		{
-			div.style.display = "none";
+			undo(after, div);
 		});
 	});
 });
